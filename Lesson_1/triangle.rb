2333df799e68197b5ae28b341d1350b3d8a6ceb2
sanjar-kazakov <93393747+sanjar-kazakov@users.noreply.github.com>
2024-02-01 22:30:21 +0100

    Delete Lesson_1 directory

--- Lesson_1/triangle.rb
+++
@@ -1,9 +0,0 @@
-puts "To calculate the area of a triangle, we need to know its dimensions."
-puts "First, enter the length of its base:"
-base = gets.to_f
-
-puts "Now, enter its height:"
-height = gets.to_f
-
-triangle_area = 1.0/2 * base * height
-puts "The are of this triangle is: #{triangle_area}"
